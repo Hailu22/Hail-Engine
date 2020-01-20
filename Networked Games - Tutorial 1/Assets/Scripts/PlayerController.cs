663a03c4c88a3a5182883da23d6900e6a4b0d0b0
Hailu22 <sohails1996.ss@gmail.com>
2020-01-20 16:37:44 +0000

    Initial comit

---
+++ Networked Games - Tutorial 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    const int MOVEMENT_RATE = 30;
+    const int ROTATION_RATE = 150;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * ROTATION_RATE;
+        var verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * MOVEMENT_RATE;
+
+        transform.Rotate(0, horizontalInput, 0);
+        transform.Translate(0, 0, verticalInput);
+    }
+}
